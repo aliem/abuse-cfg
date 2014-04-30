694d259ce5e721783ac1188d10f69437401ee0bb
Lorenzo Giuliani <lorenzo@getcontentment.com>
2014-04-30 15:43:31 +0100

    taping a few more tests

--- tests.js
+++ tests.js
@@ -1,6 +1,34 @@
+var test = require('tape')
+var path = require('path')
+
 var config = require('./')
-config.load('package', './package.json')
+config.load('package', path.resolve(path.join(__dirname, 'package.json')))
 
-console.assert(config.package.version === "0.0.1", "Someone changed version")
+test("load of a json file", function (t) {
+  t.plan(1)
+  t.equal(config.package.version, "0.0.1")
+})
 
-console.log('done.');
+test("submodules should have the save configuration", function (t) {
+  t.plan(4)
+
+  t.deepEqual(config.package, suba(), "Configuration inside new namespaces should have the same properties")
+  t.deepEqual(config.package, subb(), "Configuration inside new namespaces should have the same properties at any time")
+
+  config.package.version = "1.3.37"
+  t.equal(suba().version, "1.3.37", "Changing a property should reflect")
+  t.equal(subb().version, "1.3.37", "Changing a property should reflect also in old includes")
+})
+
+function suba () {
+  var config = require('./')
+  return config.package
+}
+
+var subb = (function () {
+  var config = require('./')
+
+  return function () {
+    return config.package
+  }
+})()
